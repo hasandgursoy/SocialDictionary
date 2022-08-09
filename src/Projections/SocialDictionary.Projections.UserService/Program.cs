5a6fe6067d1fd42bd5dbef2ecaf319547ad2bd3d
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-09 20:32:05 +0300

    Onion Architecture

---
+++ src/Projections/SocialDictionary.Projections.UserService/Program.cs
@@ -0,0 +1,10 @@
+using SocialDictionary.Projections.UserService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
